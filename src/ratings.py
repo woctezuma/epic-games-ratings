2cf5e0b9912435d8d38f6215b2d0e87aeeca994c
Wok <woctezuma@users.noreply.github.com>
2022-07-26 00:49:54 +0200

    Add utils to download or load game ratings

---
+++ src/ratings.py
@@ -0,0 +1,32 @@
+from src.disk_utils import save_json, get_game_ratings_fname, load_json
+from src.query_game_rating import to_game_rating
+
+
+def download_game_ratings(sandbox_ids, save_every=60, verbose=True):
+    game_ratings = load_game_ratings()
+
+    for iter, id in enumerate(sandbox_ids, start=1):
+        if id in game_ratings and game_ratings[id] is not None:
+            continue
+
+        rating = to_game_rating(id, verbose=verbose)
+
+        if verbose and id is not None:
+            print(f"{id} -> {rating}")
+
+        game_ratings[id] = rating
+
+        if iter % save_every == 0:
+            save_json(game_ratings, get_game_ratings_fname())
+
+    save_json(game_ratings, get_game_ratings_fname())
+
+    return game_ratings
+
+
+def load_game_ratings():
+    try:
+        game_ratings = load_json(get_game_ratings_fname())
+    except FileNotFoundError:
+        game_ratings = dict()
+    return game_ratings
