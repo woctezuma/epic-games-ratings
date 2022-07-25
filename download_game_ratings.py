c57dede4ccd626a6641a7b6f4dc801666997100a
Wok <woctezuma@users.noreply.github.com>
2022-07-26 01:29:17 +0200

    Minor refactoring: rename variable
    From sandbox_dict to sandbox_ids_dict.

--- download_game_ratings.py
+++ download_game_ratings.py
@@ -3,8 +3,8 @@ from src.sandboxes import load_sandbox_ids_dict
 
 
 def main():
-    sandbox_dict = load_sandbox_ids_dict()
-    sandbox_ids = set(sandbox_dict.values())
+    sandbox_ids_dict = load_sandbox_ids_dict()
+    sandbox_ids = set(sandbox_ids_dict.values())
 
     game_ratings = download_game_ratings(sandbox_ids, verbose=True)
 
