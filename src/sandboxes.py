bdf2ab56c6660ef207ff5a13d54303119bf56bdb
Wok <woctezuma@users.noreply.github.com>
2022-07-25 23:29:12 +0200

    Add a separate function to load sandbox Ids

--- src/sandboxes.py
+++ src/sandboxes.py
@@ -3,10 +3,7 @@ from src.query_sandbox_id import to_sandbox_id
 
 
 def download_sandbox_ids(slugs, save_every=60, verbose=True):
-    try:
-        sandbox_ids = load_json(get_sandbox_ids_fname())
-    except FileNotFoundError:
-        sandbox_ids = dict()
+    sandbox_ids = load_sandbox_ids()
 
     for iter, s in enumerate(slugs, start=1):
         if s in sandbox_ids and sandbox_ids[s] is not None:
@@ -24,4 +21,12 @@ def download_sandbox_ids(slugs, save_every=60, verbose=True):
 
     save_json(sandbox_ids, get_sandbox_ids_fname())
 
+    return sandbox_ids
+
+
+def load_sandbox_ids():
+    try:
+        sandbox_ids = load_json(get_sandbox_ids_fname())
+    except FileNotFoundError:
+        sandbox_ids = dict()
     return sandbox_ids
