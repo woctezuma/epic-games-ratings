1dec9512cfcee9fdc2cdb5ccb607b65bdc491546
Wok <woctezuma@users.noreply.github.com>
2022-07-25 22:36:56 +0200

    Clean up unused code for GET requests

--- src/api.py
+++ src/api.py
@@ -5,15 +5,6 @@ def get_graphql_api_url(subdomain="graphql"):
     return f"https://{subdomain}.epicgames.com/graphql"
 
 
-def get_store_api_url():
-    return get_graphql_api_url(subdomain="store")
-
-
-def send_get_to_api(params, subdomain="graphql", verbose=True):
-    r = requests.get(get_graphql_api_url(subdomain=subdomain), params=params)
-    return to_data(r, verbose=verbose)
-
-
 def send_post_to_api(json_data, subdomain="graphql", verbose=True):
     r = requests.post(get_graphql_api_url(subdomain=subdomain), json=json_data)
     return to_data(r, verbose=verbose)
