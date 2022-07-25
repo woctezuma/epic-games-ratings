d7c6403e90c674ae7c5c138d1ca26c3157cf6a72
Wok <woctezuma@users.noreply.github.com>
2022-07-26 00:50:29 +0200

    Add main() to download game ratings from sandbox ids

--- download_game_ratings.py
+++ download_game_ratings.py
@@ -1,4 +1,12 @@
+from src.ratings import download_game_ratings
+from src.sandboxes import load_sandbox_ids
+
+
 def main():
+    sandbox_ids = load_sandbox_ids()
+
+    game_ratings = download_game_ratings(sandbox_ids, verbose=True)
+
     return
 
 
