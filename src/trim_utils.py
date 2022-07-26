ed30d55ca40ebe4358140537957782652a6207b1
Wok <woctezuma@users.noreply.github.com>
2022-07-26 21:00:07 +0200

    Explain the sort in a comment

--- src/trim_utils.py
+++ src/trim_utils.py
@@ -22,6 +22,7 @@ def trim_sandbox_ids_dict(sandbox_ids_dict):
 
     trimmed_dict = dict()
 
+    # NB: the sort aims to **try** to make the base edition of the game appear first. This is useful, yet not foolproof!
     for slug, sandbox_id in sorted(sandbox_ids_dict.items(), key=lambda x: x[0]):
         # NB: the check for positive length is to tackle the edge case where store data is not on the disk.
         if slug not in slugs_for_base_games and len(slugs_for_base_games) > 0:
