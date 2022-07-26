27a2b4f145e376cfde3132b72d6f96d68fe18011
Wok <woctezuma@users.noreply.github.com>
2022-07-26 08:59:38 +0200

    Clarify check for the validity of a sandbox id

--- src/sandboxes.py
+++ src/sandboxes.py
@@ -2,11 +2,17 @@ from src.disk_utils import save_json, get_sandbox_ids_fname, load_json
 from src.query_sandbox_id import to_sandbox_id
 
 
+def is_a_valid_sandbox_id(sandbox_id):
+    is_valid = bool(sandbox_id is not None)
+
+    return is_valid
+
+
 def download_sandbox_ids_dict(slugs, save_every=60, verbose=True):
     sandbox_ids_dict = load_sandbox_ids_dict()
 
     for iter, s in enumerate(slugs, start=1):
-        if s in sandbox_ids_dict and sandbox_ids_dict[s] is not None:
+        if s in sandbox_ids_dict and is_a_valid_sandbox_id(sandbox_ids_dict[s]):
             continue
 
         id = to_sandbox_id(s, verbose=verbose)
